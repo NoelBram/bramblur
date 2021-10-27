d1f78d8f2a7c6eddb47c8e42505f9ecee8b1ed8a
Noel Brambila <noelbrambila@yahoo.com>
2021-10-27 11:45:28 -0700

    first try with sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,142 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/rendering.dart';
+import 'package:bramblur/signin_button.dart';
+import 'package:bramblur/login_controller.dart';
+import 'package:bramblur/tracking_text_input.dart';
 
-import 'screens/overview_screen.dart';
-import 'screens/splash_screen.dart';
-import '/screens/auth_screen.dart';
-import '/screens/overview_screen.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // ignore: deprecated_member_use
-  SystemChrome.setEnabledSystemUIOverlays([]);
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitDown,
-    DeviceOrientation.portraitUp,
-  ]);
-  await Firebase.initializeApp();
-
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyApp(),
-  ));
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Auth Application',
-        theme: ThemeData(
-            primarySwatch: Colors.pink,
-            backgroundColor: Colors.white,
-            // ignore: deprecated_member_use
-            accentColor: Colors.deepPurple,
-            // ignore: deprecated_member_use
-            accentColorBrightness: Brightness.dark,
-            buttonTheme: ButtonTheme.of(context).copyWith(
-                buttonColor: Colors.pink,
-                textTheme: ButtonTextTheme.primary,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ))),
-        home: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (ctx, userSnapshot) {
-            if (userSnapshot.connectionState == ConnectionState.waiting) {
-              return const SplashScreen();
-            }
-            if (userSnapshot.hasData) {
-              return const OverviewScreen();
-            }
-            return const AuthScreen();
-          },
-        ));
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late LoginController _bramblurController;
+  @override
+  initState() {
+    _bramblurController = LoginController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    EdgeInsets devicePadding = MediaQuery.of(context).padding;
+
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(93, 142, 155, 1.0),
+      body: Row(
+        children: <Widget>[
+          Positioned.fill(
+            child: Container(
+              decoration: const BoxDecoration(
+                // Box decoration takes a gradient
+                gradient: LinearGradient(
+                  // Where the linear gradient begins and ends
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  // Add one stop for each color. Stops should increase from 0 to 1
+                  stops: [0.0, 1.0],
+                  colors: [
+                    Color(0xff00BFA5),
+                    Color(0xff64FFDA),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.only(
+                  left: 20.0, right: 20.0, top: devicePadding.top + 50.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      height: 200,
+                      padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+                      child: FlareActor(
+                        "assets/Teddy.flr",
+                        shouldClip: false,
+                        alignment: Alignment.bottomCenter,
+                        fit: BoxFit.contain,
+                        controller: _bramblurController,
+                      )),
+                  Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(25.0),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Form(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            TrackingTextInput(
+                              label: "Email",
+                              hint: "What's your email address?",
+                              onCaretMoved: (Offset? caret) {
+                                _bramblurController.lookAt(caret);
+                              },
+                            ),
+                            TrackingTextInput(
+                              label: "Password",
+                              hint: "I'm not watching",
+                              isObscured: true,
+                              onCaretMoved: (Offset? caret) {
+                                _bramblurController.coverEyes(caret != null);
+                                _bramblurController.lookAt(null);
+                              },
+                              onTextChanged: (String value) {
+                                _bramblurController.setPassword(value);
+                              },
+                            ),
+                            SigninButton(
+                              child: const Text("Sign In",
+                                  style: TextStyle(
+                                      fontFamily: "OpenSans",
+                                      fontSize: 16,
+                                      color: Colors.white)),
+                              onPressed: () {
+                                _bramblurController.submitPassword();
+                              },
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
